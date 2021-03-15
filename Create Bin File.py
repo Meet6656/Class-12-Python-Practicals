db4e6dba2286ab4e5f67732933fea757567bfb04
Meet6656 <meetparmar6656@gmail.com>
2021-03-15 21:52:33 +0530

    create binary file

---
+++ Create Bin File.py
@@ -0,0 +1,16 @@
+import pickle
+stu={}
+stulist=[]
+n = int(input("Enter the number of students: "))
+for i in range(n):
+    stu['roll_no']=int(input('Enter roll number of the student: '))
+    stu['name']=input("Enter the name of the student: ")
+    stulist.append(stu)
+    stu = {}
+stufile = open("Student.BIN", 'ab+')
+pickle.dump(stulist, stufile)
+stufile.close()
+print("Record added successfully")
+read= open("Student.BIN", 'rb')
+list = pickle.load(read)
+print(list)
