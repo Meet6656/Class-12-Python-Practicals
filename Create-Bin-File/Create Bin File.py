7a762534949d59be66c5fc9373372e759784d460
Meet6656 <meetparmar6656@gmail.com>
2021-03-16 21:26:48 +0530

    Edited Code README.md

--- Create-Bin-File/Create Bin File.py
+++ Create-Bin-File/Create Bin File.py
@@ -6,11 +6,11 @@ for i in range(n):
     stu['roll_no']=int(input('Enter roll number of the student: '))
     stu['name']=input("Enter the name of the student: ")
     stulist.append(stu)
-    stu = {}
+    stu = []
 stufile = open("Student.BIN", 'ab+')
 pickle.dump(stulist, stufile)
 stufile.close()
 print("Record added successfully")
 file= open("Student.BIN", 'rb')
-list = file.read()
+list =file.read()
 print(list)
